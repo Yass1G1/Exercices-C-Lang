9861ccfc62fecf611779e1b0062a6d724f3e5e87
Yass <67269173+Yass1G1@users.noreply.github.com>
2022-08-09 01:33:16 +0200

    The Guessing Game
    Adding little description concerning the Guessing Game

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+// Guessing game in C, the goal is to find the number chosen by the computer, which is a number between 1 and 100, good luck !!
 
 
 int main()
